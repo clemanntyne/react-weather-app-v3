6c828af283d4b001ce34199589da8b392555d29e
clemanntyne <92274079+clemanntyne@users.noreply.github.com>
2022-02-08 18:03:06 -0800

    Added Axios and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
+      <Weather />
       <footer>
-        Project coded by Stephanie Clemann. Is <a href="">Open Source</a> on
-        GitHub.
+        Project coded by Stephanie Clemann. Is{" "}
+        <a
+          href="https://github.com/clemanntyne/react-weather-app-v3"
+          target="_blank"
+        >
+          Open Source
+        </a>{" "}
+        on GitHub.
       </footer>
     </div>
   );
