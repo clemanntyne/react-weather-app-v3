a2bea3620a4d09b867fb4165d21707c3012de7b3
clemanntyne <92274079+clemanntyne@users.noreply.github.com>
2022-03-05 11:59:31 -0800

    Added basic html | Current weather info display

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
-      <Weather />
-      <footer>
-        Project coded by Stephanie Clemann. Is{" "}
-        <a
-          href="https://github.com/clemanntyne/react-weather-app-v3"
-          target="_blank"
-        >
-          Open Source
-        </a>{" "}
-        on GitHub.
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          Project coded by Stephanie Clemann. Is{" "}
+          <a
+            href="https://github.com/clemanntyne/react-weather-app-v3"
+            target="_blank"
+          >
+            Open Source
+          </a>{" "}
+          on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,47 @@
 import React from "react";
 import axios from "axios";
 
+import "./Weather.css";
+
 export default function Weather() {
-  return <p>Hello</p>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="City Name..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
+      <h1>San Francisco</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="http://openweathermap.org/img/wn/02d@2x.png"
+            alt="mostly cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Percipitation: 15%</li>
+            <li>Humidity 72%</li>
+            <li>Wind:13 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
